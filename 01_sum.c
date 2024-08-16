99d4fa53c6346e5edd60a76a193106c4640ef332
DaniBencz <daniel.bencz@gmail.com>
2024-08-16 11:07:21 +0200

    first commit

---
+++ 01_sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    printf("Sum of a and b is %d snd the meaning of life is %d\n", c, 42);
+
+    return 0;
+}
