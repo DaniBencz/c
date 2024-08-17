6d523668971cd22963a25084acc7edf464e51d8e
Daniel Bencz <daniel.bencz@byborgenterprises.com>
2024-08-17 14:51:03 +0200

    boolean

---
+++ 06_boolean.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool val1 = true;
+    bool val2 = false;
+    printf("val1 is %d and val2 is %d\n", val1, val2);
+
+    bool gd = 5 > 1;
+    printf("5 > 1 = %d\n", gd);
+
+    return 0;
+}
